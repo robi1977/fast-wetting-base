fcbc6b85bca77d69c6ebac96b49c322b02f267a8
Rafal Nowak <rafal.robi.nowak@gmail.com>
2021-05-11 15:30:22 +0200

    Added routes for SampleCOntroller

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\SampleController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -20,3 +20,4 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::resource('sample', SampleController::class)->middleware('auth');
